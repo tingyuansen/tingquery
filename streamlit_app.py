c418ecd89af1a0cf5303f349352c0afd2611a96d
Yuan-Sen Ting <tingyuansen@users.noreply.github.com>
2024-04-13 15:50:56 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,51 +1,46 @@
 import streamlit as st
-import json
+from langchain.llms import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
 
-# Load your JSON data
-def load_data(filename):
-    with open(filename, "r") as file:
-        data = json.load(file)
-    return data
+def generate_response(uploaded_file, openai_api_key, query_text):
+    # Load document if file is uploaded
+    if uploaded_file is not None:
+        documents = [uploaded_file.read().decode()]
+        # Split documents into chunks
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.create_documents(documents)
+        # Select embeddings
+        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+        # Create a vectorstore from documents
+        db = Chroma.from_documents(texts, embeddings)
+        # Create retriever interface
+        retriever = db.as_retriever()
+        # Create QA chain
+        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+        return qa.run(query_text)
 
-# Function to get compressed text by arXiv ID
-def get_compressed_text_by_id(data, arxiv_id):
-    for entry in data['arxiv-id'].items():
-        if entry[1] == arxiv_id:
-            index = entry[0]
-            compressed_text = data['compressed_text'][index]
-            return compressed_text
-    return None
+# Page title
+st.set_page_config(page_title='🦜🔗 Ask the Doc App')
+st.title('🦜🔗 Ask the Doc App')
 
-# Streamlit interface
-def main():
-    st.title("Astrobytes")
-    st.markdown("### - the astro-ph machine's digest")
-    st.markdown("")
+# File upload
+uploaded_file = st.file_uploader('Upload an article', type='txt')
+# Query text
+query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
-    # Load data
-    data = load_data('./astro-ph.ga_streamlit.json')
+# Form input and query
+result = []
+with st.form('myform', clear_on_submit=True):
+    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
+    if submitted and openai_api_key.startswith('sk-'):
+        with st.spinner('Calculating...'):
+            response = generate_response(uploaded_file, openai_api_key, query_text)
+            result.append(response)
+            del openai_api_key
 
-    # Dropdown menu of arXiv IDs
-    arxiv_ids = list(data['arxiv-id'].values())
-    dropdown_selection = st.selectbox("Select an arXiv ID from the dropdown:", arxiv_ids)
-
-    # Entry box for arXiv ID
-    text_input_selection = st.text_input("...or enter an arXiv ID:")
-
-    # Determine the arXiv ID to use
-    arxiv_id = text_input_selection if text_input_selection else dropdown_selection
-
-    # Show the corresponding compressed text and provide a link to the PDF
-    if arxiv_id:
-        compressed_text = get_compressed_text_by_id(data, arxiv_id)
-
-        if compressed_text:
-            st.markdown(compressed_text, unsafe_allow_html=True)
-
-            arxiv_url = f"https://arxiv.org/pdf/{arxiv_id}.pdf"
-            st.markdown(f"**arXiv Paper PDF:** [View/Download PDF]({arxiv_url})", unsafe_allow_html=True)
-        else:
-            st.write("No data found for this arXiv ID")
-
-if __name__ == "__main__":
-    main()
+if len(result):
+    st.info(response)
